98ca8c6a92d01aa931d855f65482675bbec5c4d2
Thomas Brannan <th.c.brannan@gmail.com>
2020-12-04 21:10:24 -0800

    Implemented changes made after working on my schemas in a meeting with Susan

--- DataBase/schema.sql
+++ DataBase/schema.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS gallery;
+CREATE DATABASE IF NOT EXISTS mortgage;
 -- WITH
 --    [OWNER =  admin]
 --    [TEMPLATE = template]
@@ -8,33 +8,41 @@ CREATE DATABASE IF NOT EXISTS gallery;
 --    [CONNECTION LIMIT = -1] -- huhh
 --    [IS_TEMPLATE = false ];
 
-DROP TABLE IF EXISTS PropertyPrice;
-DROP TABLE IF EXISTS MortgageAgents;
-DROP TABLE IF EXISTS MortgageAgentAppointments;
+DROP TABLE IF EXISTS Properties;
+DROP TABLE IF EXISTS Agents;
+DROP TABLE IF EXISTS Appointments;
 
-CREATE TABLE IF NOT EXISTS PropertyPrices (
-  id SERIAL NOT NULL PRIMARY KEY,
-  homePrice INTEGER NOT NULL, -- CHECK ( price > 0 AND price < 1000000000),
+CREATE TABLE IF NOT EXISTS Properties (
+  property_id SERIAL NOT NULL PRIMARY KEY,
+  PRIMARY KEY(property_id),
+  homePrice INTEGER NOT NULL -- CHECK ( price > 0 AND price < 1000000000)
 );
 
-CREATE TABLE IF NOT EXISTS MortgageAgents (
+CREATE TABLE IF NOT EXISTS Agents (
+  agent_id SERIAL NOT NULL,
+  PRIMARY KEY(agent_id),
   name VARCHAR(50) NOT NULL,
-  title VARCHAR(50) NOT NULL,
   title VARCHAR(50) NOT NULL,
   rating INTEGER NOT NULL, -- CHECK ( rating > 0 AND rating <= 5),
   recentSales INTEGER NOT NULL, -- CHECK ( recentSales >= 0 AND recentSales <= 1000),
   phone VARCHAR(50) NOT NULL,
   avatar VARCHAR(50) NOT NULL,
   about VARCHAR(50) NOT NULL,
-  agency VARCHAR(50) NOT NULL,
+  agency VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS MortgageAgentAppointments (
+CREATE TABLE IF NOT EXISTS Appointments (
+  CONSTRAINT agent_key
+    FOREIGN KEY (Properties)
+      REFERENCES property_id(Properties),
+  CONSTRAINT agent_key
+    FOREIGN KEY (Agents)
+      REFERENCES agent_id(Agents),
   inPerson BOOLEAN,
   date VARCHAR(50) NOT NULL,
   time VARCHAR(50) NOT NULL,
   name VARCHAR(50) NOT NULL,
   phone VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
-  financing BOOLEAN,
+  financing BOOLEAN
 );
