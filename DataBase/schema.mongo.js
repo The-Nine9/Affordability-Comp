c307c5b030a62dcbcea685701127b116f5fc6ba2
Thomas Brannan <th.c.brannan@gmail.com>
2020-12-04 20:27:49 -0800

    Brought this into line with the 2-schema approach Susan and I came up with in our meeting today. Property { 	appointments: [ {name: 1}, {name: 2}] } Agents { 	appointments: [{}, {}] } eliminate mortageagentappointmentmodel

--- DataBase/schema.mongo.js
+++ DataBase/schema.mongo.js
@@ -1,31 +1,22 @@
 const mongoose = require('mongoose');
 
-const HomePriceModel = mongoose.model('Price', new mongoose.Schema({ id: Number, homePrice: Number }));
-
-const MortgageAgentModel = mongoose.model('Agent', new mongoose.Schema({
-  name: String,
-  title: String,
-  rating: Number,
-  recentSales: Number,
-  phone: String,
-  avatar: String,   // s3 resource
-  about: String,    // a description
-  agency: String,   // employer
+const Property = mongoose.model("Property", new mongoose.Schema({
+  "id": Number,
+  "price": Number,
+  "appointments": Array
 }));
 
-const MortgageAgentAppointmentModel = mongoose.model('Schedule', new mongoose.Schema({
-  inPerson: Boolean,
-  date: String,
-  time: String,
-  name: String,
-  phone: String,
-  email: String,
-  financing: Boolean,
+const Agent = mongoose.model("Agent", new mongoose.Schema({
+  "name": String,
+  "title": String,
+  "rating": Number,
+  "recentSales": Number,
+  "phone": String,
+  "avatar": String,   // s3 resource
+  "about": String,    // a description
+  "agency": String,   // employer
+  "appointments": Array
 }));
 
-module.exports.PropertyPrice = PropertyPriceModel;
-module.exports.MortgageAgent = MortgageAgentModel;
-module.exports.MortgageAgentAppointment = MortgageAgentAppointmentModel;
-
-// This is just OK... consider denormalization
-// one table for everything? one query for all data about a property --> faster, much more space taken (agents+etc. duplicated across properties)
+module.exports.Property = PropertyPriceModel;
+module.exports.Agent = MortgageAgentModel;
