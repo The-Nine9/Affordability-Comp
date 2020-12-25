53e0c6493e5311f6fef76973f863b0a0b5162d31
Thomas Brannan <th.c.brannan@gmail.com>
2020-12-24 19:19:39 -0800

    Refactored to mongodb node driver

--- DataBase/mongo/controllers/join.js
+++ DataBase/mongo/controllers/join.js
@@ -1,6 +1,9 @@
-const mongoose = require("mongoose");
-const mongo = require("mongodb");
-const { Agent, Property } = require("../schema.mongo.js");
+// https://docs.mongodb.com/drivers/node/fundamentals/connection
+// https://docs.mongodb.com/drivers/node/usage-examples/find
+
+const { MongoClient } = require('mongodb');
+const url = "mongodb://localhost:27017/"; // I've left mongo on its default port
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
 module.exports.createAll = async(property, agents, callback) => {
   let err = null;
@@ -36,99 +39,111 @@ module.exports.createAll = async(property, agents, callback) => {
 };
 
 module.exports.readAll = async (property_id, callback) => {
-  let property;
+  property_id = Number(property_id);
+  let property = {};
   let agents = [];
   let err = null;
+  await client.connect({ useNewUrlParser: true });
+  const mortgage = client.db("mortgage");
+  const properties_collection = mortgage.collection("properties");
+  const agents_collection = mortgage.collection("agents");
   try {
-    property = await Property.findOne({property_id});
+
+    // await client.db("mortgage").command({ ping: 1 });
+
+    property = await properties_collection.findOne({ property_id });
+    console.log(`>>>>>>>>\nproperty_id\n>>>>>>>>\n${property_id}\t${typeof property_id}\nproperty\n>>>>>>>>\n${property}\n>>>>>>>>`);
+
     if (property.agents.length) {
       agents = await Promise.all(property.agents.map(async (agent_id) => {
-        return await Agent.findOne({agent_id});
+        agent_id = Number(agent_id);
+        return await agents_collection.findOne({agent_id});
       }));
     }
-  } catch(e) {
-    err = e;
-  } finally {
-    if (err) {
-      callback(err, null);
-    } else {
-      let res = {
-        "property": property,
-        "agents": agents
-      };
-      callback(null, res);
-    }
-  }
-};
 
-
-module.exports.create = async (agent_id, property_id, callback) => {
-  // Search properties by id
-  // Search agents by id
-  // update property with agent's id
-  // update agent with proeprty's id
-  let agent;
-  let property;
-  let err = null;
-  try {
-
-    property = await Property.findOne({"property_id": property_id});
-    if (!property.agents) {
-      property.agents = [];
-    }
-    property.agents.push(agent_id);
-    await property.save();
-
-    agent = await Agent.findOne({"agent_id": agent_id});
-    if (!agent.properties) {
-      agent.properties = [];
-    }
-    agent.properties.push(property_id);
-    await agent.save();
+    // property = await Property.findOne({property_id});
+    // if (property.agents.length) {
+    //   agents = await Promise.all(property.agents.map(async (agent_id) => {
+    //     return await Agent.findOne({agent_id});
+    //   }));
+    // }
 
   } catch(e) {
     err = e;
   } finally {
-    if (err) {
-      callback(err);
-    } else {
-      callback(null);
-    }
+    await client.close();
+    callback(err, {property, agents});
   }
 };
 
-module.exports.delete = async (agent_id, property_id, callback) => {
-  // locate property by address AND name of agent
-  // locate agent by name AND id of property
-  agent_id = Number(agent_id);
-  property_id = Number(property_id);
-  let property;
-  let agent;
-  let err = null;
-  try {
+// unused routes
+// module.exports.create = async (agent_id, property_id, callback) => {
+//   // Search properties by id
+//   // Search agents by id
+//   // update property with agent's id
+//   // update agent with proeprty's id
+//   let agent;
+//   let property;
+//   let err = null;
+//   try {
 
-    agent = await Agent.findOne({"agent_id": agent_id});
-    agent.properties = agent.properties.filter(id => id !== property_id);
+//     property = await Property.findOne({"property_id": property_id});
+//     if (!property.agents) {
+//       property.agents = [];
+//     }
+//     property.agents.push(agent_id);
+//     await property.save();
 
-    property = await Property.findOne({"property_id": property_id});
-    property.agents = property.agents.filter(id => id !== agent_id);
+//     agent = await Agent.findOne({"agent_id": agent_id});
+//     if (!agent.properties) {
+//       agent.properties = [];
+//     }
+//     agent.properties.push(property_id);
+//     await agent.save();
 
-    // console.log(`@param agent_id = ${agent_id} <${typeof agent_id}>`);
-    // console.log(`@param property_id = ${property_id} <${typeof property_id}>`);
-    // console.log(`agent.properties:\n${agent.properties}`);
-    // console.log(`property.agents:\n${property.agents}`);
+//   } catch(e) {
+//     err = e;
+//   } finally {
+//     if (err) {
+//       callback(err);
+//     } else {
+//       callback(null);
+//     }
+//   }
+// };
 
-    await agent.save();
-    await property.save();
+// module.exports.delete = async (agent_id, property_id, callback) => {
+//   // locate property by address AND name of agent
+//   // locate agent by name AND id of property
+//   agent_id = Number(agent_id);
+//   property_id = Number(property_id);
+//   let property;
+//   let agent;
+//   let err = null;
+//   try {
 
-  } catch(e) {
-    err = e;
-  } finally {
-    if (err) {
-      callback(err);
-    } else {
-      callback(null);
-    }
-  }
-};
+//     agent = await Agent.findOne({"agent_id": agent_id});
+//     agent.properties = agent.properties.filter(id => id !== property_id);
+
+//     property = await Property.findOne({"property_id": property_id});
+//     property.agents = property.agents.filter(id => id !== agent_id);
+
+//     // console.log(`@param agent_id = ${agent_id} <${typeof agent_id}>`);
+//     // console.log(`@param property_id = ${property_id} <${typeof property_id}>`);
+//     // console.log(`agent.properties:\n${agent.properties}`);
+//     // console.log(`property.agents:\n${property.agents}`);
+
+//     await agent.save();
+//     await property.save();
+
+//   } catch(e) {
+//     err = e;
+//   } finally {
+//     if (err) {
+//       callback(err);
+//     } else {
+//       callback(null);
+//     }
+//   }
+// };
 
